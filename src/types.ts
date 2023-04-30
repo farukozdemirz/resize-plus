0de116d189b863f3975548af993534d95f76a698
farukozdemirz <frk.ozdmrr@gmail.com>
2023-04-30 09:51:56 +0300

    change config & types

--- src/types.ts
+++ src/types.ts
@@ -9,40 +9,30 @@ export interface Enable {
   topLeft?: boolean;
 }
 export interface IStyle {
-  width: string | number;
-  height: string | number;
-  left?: string | number;
-  top?: string | number;
-  angle?: string | number;
+  width: number;
+  height: number;
+  left: number;
+  top: number;
+  angle: number;
 }
 export interface ResizerProps {
   minWidth?: number;
   minHeight?: number;
-  aspect: string;
+  aspectRatio: string;
   resizeFromCenter: boolean;
   lockAspectRatio: boolean;
   onResizeStart: () => void;
   onResizeEnd: () => void;
   onResize: () => void;
-  onRotateStart: () => void;
+  onRotateStart: (e: any) => void;
   onRotateEnd: () => void;
   onRotate: () => void;
   isHideOnResize: boolean;
   onResizerHide: () => void;
   onResizerShown: () => void;
   enable: Enable;
-  style?: IStyle;
+  style: IStyle;
   children: React.ReactNode;
   zoom: number;
+  isLocked?: boolean;
 }
-// resizers: {
-//   n: true,                // top middle resize handler            true:visible|false:hidden
-//   s: true,                // bottom middle resize handler
-//   e: true,                // right middle resize handler
-//   w: true,                // left middle resize handler
-//   ne: true,               // top-right resize handler
-//   nw: true,               // top-left resize handler
-//   se: true,               // bottom-right resize handler
-//   sw: true,               // bottom-left resize handler
-//   r: true,                // rotate handler
-// },
